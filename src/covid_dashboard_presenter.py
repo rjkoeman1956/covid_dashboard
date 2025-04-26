6c6358c1e9d1f4f35895e1189c98afd1f912b305
Ronald Koeman <63474426+rjkoeman1956@users.noreply.github.com>
2025-04-26 23:48:31 +0200

    Add files via upload

--- src/covid_dashboard_presenter.py
+++ src/covid_dashboard_presenter.py
@@ -1,1 +1,254 @@
-# covid_dashboard_presenter.py - placeholder
+#%matplotlib inline
+"""
+TODO: Add module-level description here.
+"""
+
+import matplotlib.pyplot as plt
+import pandas as pd_riool
+import geopandas as gpd
+from pathlib import Path
+import ipywidgets as widgets
+import seaborn as sns
+import numpy as np
+
+def update_plot(df, year, total_reported, hospital_admission, deceased, province, municipalities, months):
+    """
+    TODO: Describe what this function does.
+    """
+    """
+    Generates a grouped bar chart with Covid-data upon GUI input.
+    """
+    # Filter by year
+    filtered_df = df[df['Year'] == year]
+
+    # Aggregation based upon input
+    if province == 'Netherlands':
+        if months:
+            grouped_df = filtered_df.groupby(['Month', 'Month_name'])[
+                ['Total_reported', 'Hospital_admission', 'Deceased']].sum().sort_values(by='Month')
+            grouped_df.reset_index(level=0, drop=True, inplace=True)
+        else:
+            grouped_df = filtered_df.groupby('Year')[['Total_reported', 'Hospital_admission', 'Deceased']].sum()
+    elif province == 'All provinces':
+        grouped_df = filtered_df.groupby(['Province_merged'])[
+            ['Total_reported', 'Hospital_admission', 'Deceased']].sum()
+    elif municipalities:
+        filtered_df = filtered_df[filtered_df['Province_merged'] == province]
+        grouped_df = filtered_df.groupby(['Municipality_name_merged'])[
+            ['Total_reported', 'Hospital_admission', 'Deceased']].sum()
+    elif months:
+        filtered_df = filtered_df[filtered_df['Province_merged'] == province]
+        grouped_df = filtered_df.groupby(['Month', 'Month_name'])[
+            ['Total_reported', 'Hospital_admission', 'Deceased']].sum()
+        grouped_df.reset_index(level=0, drop=True, inplace=True)
+    else:
+        filtered_df = filtered_df[filtered_df['Province_merged'] == province]
+        grouped_df = filtered_df.groupby(['Province_merged'])[
+            ['Total_reported', 'Hospital_admission', 'Deceased']].sum()
+
+    # Get label x values
+    x_labels = grouped_df.index
+
+    # Get bar y values
+    y_total_reported = grouped_df['Total_reported'] if total_reported else None
+    y_hospital_admission = grouped_df['Hospital_admission'] if hospital_admission else None
+    y_deceased = grouped_df['Deceased'] if deceased else None
+    
+    # Set minimal width of bars
+    x = np.arange(len(x_labels)) if len(x_labels) > 1 else np.array([0])
+    bar_width = 0.25
+
+    # Dynamic bar formatting
+    if len(x) < 10:
+        plt.figure(figsize=(15, 6))
+    elif len(x) < 15:
+        plt.figure(figsize=(20, 6))
+    elif len(x) < 20:
+        plt.figure(figsize=(24, 7))
+    elif len(x) < 25:
+        plt.figure(figsize=(24, 9))
+    elif len(x) < 30:
+        plt.figure(figsize=(26, 9))
+    else:
+        plt.figure(figsize=(28, 10))
+
+    # Plot bars next to each other
+    if y_total_reported is not None:
+        plt.bar(x - bar_width, y_total_reported, width=bar_width, color='blue', label='Covid reported')
+    if y_hospital_admission is not None:
+        plt.bar(x, y_hospital_admission, width=bar_width, color='green', label='Hospital admission')
+    if y_deceased is not None:
+        plt.bar(x + bar_width, y_deceased, width=bar_width, color='orange', label='Deceased')
+
+    # Axis-labels, title, legend
+    plt.xticks(x, x_labels, rotation=45)
+    if int(year) >= 2023:
+        plt.xlabel('Warning: Not all RIVM data is available after 1-1-2023', color='red')
+    plt.ylabel('Number')
+    plt.title(f'Covid data for {year} ({province})')
+    plt.gca().get_yaxis().get_major_formatter().set_scientific(False)
+    if any([total_reported, hospital_admission, deceased]):
+        plt.legend()
+    plt.tight_layout()
+    plt.show()
+
+def plot_province_heatmap(gdf, column='Total_reported'):
+    """
+    TODO: Describe what this function does.
+    """
+    import matplotlib.pyplot as plt
+
+    fig, ax = plt.subplots()
+    fig.set_size_inches(6, 6)
+
+    xlim = gdf.total_bounds[[0, 2]]
+    ylim = gdf.total_bounds[[1, 3]]
+
+    gdf.plot(
+        column=column,
+        cmap='OrRd',
+        linewidth=0.5,
+        ax=ax,
+        edgecolor='0.8',
+        legend=True,
+        legend_kwds={'label': column.replace('_', ' '), 'orientation': 'vertical'},
+        missing_kwds={"color": "lightgrey", "label": "No data"}
+    )
+
+    ax.set_xlim(xlim)
+    ax.set_ylim(ylim)
+    ax.set_aspect('equal')
+    ax.axis('off')
+    for spine in ax.spines.values():
+        spine.set_visible(False)
+
+    leg = ax.get_legend()
+    if leg:
+        leg.set_bbox_to_anchor((1.02, 1))
+        leg.set_title(column)
+
+    plt.subplots_adjust(left=0.01, right=0.85)
+    plt.tight_layout()
+    plt.show()
+
+def plot_municipality_heatmap(gdf, column='Total_reported'):
+    """
+    TODO: Describe what this function does.
+    """
+    import matplotlib.pyplot as plt
+
+    fig, ax = plt.subplots()
+    fig.set_size_inches(6, 6)
+
+    xlim = gdf.total_bounds[[0, 2]]
+    ylim = gdf.total_bounds[[1, 3]]
+
+    gdf.plot(
+        column=column,
+        cmap='OrRd',
+        linewidth=0.5,
+        ax=ax,
+        edgecolor='0.8',
+        legend=True,
+        legend_kwds={'label': column.replace('_', ' '), 'orientation': 'vertical'},
+        missing_kwds={"color": "lightgrey", "label": "No data"}
+    )
+
+    ax.set_xlim(xlim)
+    ax.set_ylim(ylim)
+    ax.set_aspect('equal')
+    ax.axis('off')
+    for spine in ax.spines.values():
+        spine.set_visible(False)
+
+    leg = ax.get_legend()
+    if leg:
+        leg.set_bbox_to_anchor((1.02, 1))
+        leg.set_title(column)
+
+    plt.subplots_adjust(left=0.01, right=0.85)
+    plt.tight_layout()
+    plt.show()
+
+
+def plot_province_heatmap_riool(gdf, column='RNA_flow_per_100000'):
+    """
+    TODO: Describe what this function does.
+    """
+    import matplotlib.pyplot as plt
+
+    fig, ax = plt.subplots()
+    fig.set_size_inches(6, 6)
+
+    xlim = gdf.total_bounds[[0, 2]]
+    ylim = gdf.total_bounds[[1, 3]]
+
+    gdf.plot(
+        column=column,
+        cmap='OrRd',
+        linewidth=0.5,
+        ax=ax,
+        edgecolor='0.8',
+        legend=True,
+        legend_kwds={'label': column.replace('_', ' '), 'orientation': 'vertical'},
+        missing_kwds={"color": "lightgrey", "label": "No data"}
+    )
+
+    ax.set_xlim(xlim)
+    ax.set_ylim(ylim)
+    ax.set_aspect('equal')
+    ax.axis('off')
+    for spine in ax.spines.values():
+        spine.set_visible(False)
+
+    leg = ax.get_legend()
+    if leg:
+        leg.set_bbox_to_anchor((1.02, 1))
+        leg.set_title(column)
+
+    plt.subplots_adjust(left=0.01, right=0.85)
+    plt.tight_layout()
+    plt.show()
+
+def plot_municipality_heatmap_riool(gdf, column='RNA_flow_per_100000'):
+    """
+    TODO: Describe what this function does.
+    """
+    import matplotlib.pyplot as plt
+
+    fig, ax = plt.subplots()
+    fig.set_size_inches(6, 6)
+
+    xlim = gdf.total_bounds[[0, 2]]
+    ylim = gdf.total_bounds[[1, 3]]
+
+    gdf.plot(
+        column=column,
+        cmap='OrRd',
+        linewidth=0.5,
+        ax=ax,
+        edgecolor='0.8',
+        legend=True,
+        legend_kwds={'label': column.replace('_', ' '), 'orientation': 'vertical'},
+        missing_kwds={"color": "lightgrey", "label": "No data"}
+    )
+
+    ax.set_xlim(xlim)
+    ax.set_ylim(ylim)
+    ax.set_aspect('equal')
+    ax.axis('off')
+    for spine in ax.spines.values():
+        spine.set_visible(False)
+
+    leg = ax.get_legend()
+    if leg:
+        leg.set_bbox_to_anchor((1.02, 1))
+        leg.set_title(column)
+
+    plt.subplots_adjust(left=0.01, right=0.85)
+    plt.tight_layout()
+    plt.show()
+
+
+
+
--- src/data_loader.py
+++ src/data_loader.py
@@ -1,1 +1,73 @@
-# data_loader.py - placeholder
+from config import CSV_DIR, SHAPEFILES_DIR
+import pandas as pd
+import geopandas as gpd
+
+class Dataframes:
+    def __init__(self):
+        file1 = CSV_DIR / 'COVID-19_aantallen_gemeente_per_dag.csv'
+        file2 = CSV_DIR / 'COVID-19_aantallen_gemeente_per_dag_tm_03102021.csv'
+        file3 = CSV_DIR / 'COVID-19_ziekenhuisopnames.csv'
+        file4 = CSV_DIR / 'COVID-19_ziekenhuisopnames_tm_03102021.csv'
+
+        try:
+            self.aantallen_gemeente_df1 = pd.read_csv(file1, sep=';')
+            self.aantallen_gemeente_df2 = pd.read_csv(file2, sep=';')
+            self.ziekenhuisopnames_df1 = pd.read_csv(file3, sep=';')
+            self.ziekenhuisopnames_df2 = pd.read_csv(file4, sep=';')
+        except Exception:
+            self.aantallen_gemeente_df1 = None
+
+        self.merged_clean_dataset = self.prepare_merged_dataset()
+
+    def prepare_merged_dataset(self) -> pd.DataFrame:
+        try:
+            if self.aantallen_gemeente_df1 is None:
+                raise ValueError("aantallen_gemeente_df1 is niet beschikbaar.")
+            return self.aantallen_gemeente_df1.copy()
+        except Exception:
+            return None
+
+    def set_merged_and_clean_dataset(self, df: pd.DataFrame):
+        self.merged_clean_dataset = df
+
+    def get_merged_and_clean_dataset(self) -> pd.DataFrame:
+        return self.merged_clean_dataset
+
+
+
+class Riool:
+    # Class Dataframes stores different pandas dataframes. The pandas dataframe merged_clean_dataset is the result of cleaning and enriching the original RIVM datasets
+    def __init__(self):
+        """
+        TODO: Describe what this function does.
+        """
+        # get the location where the csv files are locally stored. Use joinpath to make the path platform independent
+        file1 = CSV_DIR / 'COVID-19_rioolwaterdata.csv'
+        file2 = CSV_DIR / 'gemeenten_per_provincie.csv'
+
+        try:
+            self.aantallen_riool = pd.read_csv(file1, sep=';', parse_dates=['Date_measurement'])
+            self.gemeenten_per_provincie = pd.read_csv(file2, sep=';')
+        except:
+            self.aantallen_riool = None
+            
+        self.merged_clean_dataset_riool = None
+
+    def set_merged_and_clean_dataset_riool(self, p_merged_clean_dataset_riool: pd.DataFrame):
+        """
+        :param p_merged_clean_dataset_riool which is a pandas dataframe
+        """
+        self.merged_clean_dataset_riool = p_merged_clean_dataset_riool
+
+
+def load_province_shapefile():
+    shapefile_path = SHAPEFILES_DIR / "B1_Provinciegrenzen_van_NederlandPolygon.shp"
+    return gpd.read_file(shapefile_path)
+
+def load_municipality_shapefile():
+    shapefile_path = SHAPEFILES_DIR / "sample_gemeenten_50.gpkg"
+#    gdf = gpd.read_file(GPKG_FILE, layer="gemeenten")
+    gdf = gpd.read_file(shapefile_path)
+    return gdf
+
+
