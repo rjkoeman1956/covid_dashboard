959424fafabd9369661bf4bc1170e517661211a7
Ronald Koeman <63474426+rjkoeman1956@users.noreply.github.com>
2025-05-16 00:44:41 +0200

    Add files via upload

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ CSV_DIR = PROJECT_ROOT / 'data' / 'csv'
 SHAPEFILES_DIR = PROJECT_ROOT / 'data' / 'shapefiles'
 EXPORTS_DIR = PROJECT_ROOT / 'notebooks' / 'exports'
 
-GPKG_FILE = SHAPEFILES_DIR / "wijkenbuurten_2024_v1.gpkg"
+GPKG_FILE = SHAPEFILES_DIR / "WijkBuurtkaart_2024_v1/wijkenbuurten_2024_v1.gpkg"
 LOG_FILE = EXPORTS_DIR / "export_log.csv"
 
 src_file1 = CSV_DIR / 'COVID-19_aantallen_gemeente_per_dag.csv'
@@ -19,7 +19,8 @@ src_file4 = CSV_DIR / 'COVID-19_ziekenhuisopnames_tm_03102021.csv'
 src_file5 = CSV_DIR / 'COVID-19_rioolwaterdata.csv'
 src_file6 = CSV_DIR / 'gemeenten_per_provincie.csv'
 src_file7 = SHAPEFILES_DIR / "B1_Provinciegrenzen_van_NederlandPolygon.shp"
-src_file8 = SHAPEFILES_DIR / 'wijkenbuurten_2024_v1.gpkg'
+src_file8 = SHAPEFILES_DIR / 'WijkBuurtkaart_2024_v1/wijkenbuurten_2024_v1.gpkg'
+
 
 link1 = 'https://data.rivm.nl/data/covid-19/COVID-19_aantallen_gemeente_per_dag.csv'
 link2 = 'https://data.rivm.nl/data/covid-19/COVID-19_aantallen_gemeente_per_dag_tm_03102021.csv'
